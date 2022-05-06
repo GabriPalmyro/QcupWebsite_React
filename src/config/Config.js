fd120089931d6cfc04100a60e60bb25715e8872b
GabriPalmyro <gabripalmyro13579@gmail.com>
2022-05-05 22:55:29 -0300

    Card Jogadores Time

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,2 +1,2 @@
-// export const SERVIDOR_LARAVEL = 'https://qcup-website-api.herokuapp.com';
-export const SERVIDOR_LARAVEL = 'http://localhost:8000';
+export const SERVIDOR_LARAVEL = 'https://qcup-website-api.herokuapp.com';
+// export const SERVIDOR_LARAVEL = 'http://localhost:8000';
